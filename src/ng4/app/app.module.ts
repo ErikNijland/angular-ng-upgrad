211739a7a42d2ca54e06b2383f33be2ef5a9d953
ErikNijland <e.n.nijland@gmail.com>
2017-09-09 21:52:42 +0200

    Bootstrapping the ng1 stuff from within Angular 4

--- src/ng4/app/app.module.ts
+++ src/ng4/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {UpgradeModule} from "@angular/upgrade/static";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgradeModule: UpgradeModule) {}
+
+  ngDoBootstrap() {
+    this.upgradeModule.bootstrap(document.body, ['ng1App'], { strictDi: true })
+  }
+}
